2afa8971d13e8138cb27a6cae8866a0f28b7c3f9
Tomer <tomersamchi9249@gmail.com>
2025-06-05 15:42:21 +0300

    new line

--- HW4th.py
+++ HW4th.py
@@ -9,7 +9,7 @@ comment!!!!!!!!!"""
 #read:
 # #open a file:
 # file1=open("test.txt","r") #שם הקובץ וההרשאות שלנו
-
+#comment1 for git
 # #read the file:
 # read_content=file1.read()
 # print (read_content) #ידפיס את תוכן הטקסט
